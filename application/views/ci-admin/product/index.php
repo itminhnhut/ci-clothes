cf78cc204cd3495c2539ea2cd8e780862847b6bb
minhnhut <itnguyenhominhnhut@gmail.com>
2017-11-12 16:43:56 +0700

    backend new

---
+++ application/views/ci-admin/product/index.php
@@ -0,0 +1,12 @@
+<table id="dataProduct" class="table table-striped table-bordered dataTable no-footer" cellspacing="0" width="100%">
+   <thead>
+      <tr>
+         <th>name</th>
+         <th>price</th>
+         <th>status</th>
+         <th></th>
+         <th></th>
+         <th></th>
+      </tr>
+   </thead>
+</table>
